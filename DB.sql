659f16c8955e9c7c0234032966ce5481f6466654
joje3029 <joje3029@gmail.com>
2023-10-13 16:41:47 +0900

    삭제, 상세보기, 회원가입 만들고 DB랑 연결하고 되게함

--- DB.sql
+++ DB.sql
@@ -3,10 +3,10 @@ CREATE DATABASE JAM;
 USE JAM;
 
 CREATE TABLE article(
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
     updateDate DATETIME NOT NULL,
-    title CHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     `body` TEXT NOT NULL
 );
 
@@ -20,9 +20,31 @@ title = CONCAT('제목', RAND()),
 `body` = CONCAT('내용', RAND());
 
 
-SELECT * 
-FROM article;
+UPDATE article
+SET updateDate =NOW(),
+title = CONCAT('제목', RAND()),
+`body` = CONCAT('내용', RAND())
+WHERE id = 6;
 
+CREATE TABLE `member`(
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId VARCHAR(100) NOT NULL,
+    loginPw VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL
+);
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test 1',
+loginPw = 'test 1',
+`name` = 'test 1';
+
+
+SELECT *
+FROM `member`
 
 -- INSERT INTO article
 -- SET regDate = NOW(),
